f8699200d63bd9ee4444971fc7a9a3f0ee00a9a0
Viggo Valfridsson <viggo.valfridsson@rutin.dev>
2026-02-17 09:46:10 +0100

    Fix neovim transparency

--- .config/nvim/lua/conf/plugins.lua
+++ .config/nvim/lua/conf/plugins.lua
@@ -107,7 +107,15 @@ return {
     priority = 1000,
     opts = {
       flavour = "mocha",
+      transparent_background = true,
+      styles = {
+        floats = "transparent", -- popups / telescope / etc
+      },
     },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 
   {
